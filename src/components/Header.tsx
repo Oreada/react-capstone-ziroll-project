82f0150062dee2bf85b0cebdec67b31e6561a807
Olga Antushevich <antushevich@gmail.com>
2023-01-26 12:47:56 +0200

    feat: add routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 export function Header() {
 	return (
 		<header className="header">
-			<h2 className="header-title">Pic Some</h2>
+			<NavLink to="/" end>
+				<h2 className="header-title">Pic Some</h2>
+			</NavLink>
+			<NavLink to="/cart">
+				<i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+			</NavLink>
 		</header>
 	)
 }
