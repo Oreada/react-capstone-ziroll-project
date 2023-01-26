e45b864e9180911139f23819de8d648a588a3458
Olga Antushevich <antushevich@gmail.com>
2023-01-26 12:48:58 +0200

    feat: add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
+import { Cart } from './pages/Cart';
+import { Photos } from './pages/Photos';
 
 function App() {
   return (
     <div>
       <Header />
-      <div className='container'>
-        <h1>Home Page</h1>
-      </div>
+      {/* <div className='container'>
+        <h1>Home Page</h1> */}
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      {/* </div> */}
     </div>
   );
 }
