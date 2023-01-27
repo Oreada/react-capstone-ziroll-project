aae5a294022e264339fdd09cf1b6db262cedb570
Olga Antushevich <antushevich@gmail.com>
2023-01-27 13:58:54 +0200

    feat: add the heartIcon and the cartIcon

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import { ImageProps } from "../types/types";
 
 export function Image({ className, imgUrl }: ImageProps) {
+	const [hovered, setHovered] = useState(false);
+
+	const handleMouseEnter = () => {
+		setHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setHovered(false);
+	};
+
+	const heartIcon = hovered && <i className="ri-heart-line favorite"></i>;
+	const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+
+	console.log('hovered=', hovered);
+
 	return (
-		<div className={`${className} image-container`}>
+		<div
+			className={`${className} image-container`}
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}>
 			<img src={imgUrl} className="image-grid" alt="Something" />
+			{heartIcon}
+			{cartIcon}
 		</div>
 	)
 }
