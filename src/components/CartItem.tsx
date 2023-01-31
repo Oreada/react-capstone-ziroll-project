ded2907fe7c5cc48f0b613142a7a2aa5f098886d
Olga Antushevich <antushevich@gmail.com>
2023-01-31 15:08:07 +0200

    feat: add the hover behavior for remove icons

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,17 +1,34 @@
 import { CartItemProps } from "../types/types";
 import { ContextConsumer } from "../context";
 import { PRICE } from "../constants/constants";
+import { useState } from "react";
 
 export function CartItem({ photo }: CartItemProps) {
+	const [hovered, setHovered] = useState(false);
+	const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+
+	const handleMouseEnter = () => {
+		setHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setHovered(false);
+	};
+
 	return (
 		<ContextConsumer>
 			{(context) => (
 				<div className="cart-item">
-					<i className="ri-delete-bin-line" onClick={() => context.removePhotoFromCart(photo.id)}></i>
+					<i className={iconClassName}
+						onClick={() => context.removePhotoFromCart(photo.id)}
+						onMouseEnter={handleMouseEnter}
+						onMouseLeave={handleMouseLeave}>
+					</i>
 					<img src={photo.url} width="130px" alt="Something" />
 					<p>{`$${PRICE}`}</p>
 				</div>
-			)}
-		</ContextConsumer>
+			)
+			}
+		</ContextConsumer >
 	)
 }
